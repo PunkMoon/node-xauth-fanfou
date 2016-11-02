bfb0550d0e05b44ee7248704a809b6ba984b6204
birdplane9527 <fanfou5607@gmail.com>
2016-11-02 14:47:29 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,103 @@
+var express = require('express');
+var router = express.Router();
+var OAuth = require('mashape-oauth').OAuth;
+var consumerKey    = 'b9b92b5d183905fac8c0667440a286cd';
+var consumerSecret = 'd607ce147940d6dba52917f09d98ea2c';
+var oa = new OAuth({ 
+  accessUrl:"http://fanfou.com/oauth/access_token",
+  consumerKey:consumerKey,
+  consumerSecret:consumerSecret,
+  version:'1.0',
+  signatureMethod:'HMAC-SHA1'
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.route('/get').get(function(req,res){
+	res.render('get',{title:'GET'});
+});
+router.route('/token').get(function(req, res){
+  res.render('token', { title: 'token' });
+}).post(function(req,res,next)
+{
+	var username = req.body.username;  
+	var password= req.body.password;
+	oa.getXAuthAccessToken(username, password, function (error, oauth_token, oauth_token_secret, results) {
+	  if (error){
+	  	res.json({
+	  		'status':0,
+	  		'reason':error
+	  	});
+	  }
+	  else
+	  {
+	  	res.json({
+	  		'status':1,
+	  		'token':oauth_token,
+	  		'secret':oauth_token_secret
+	  	});
+	  }
+});
+});
+router.route('/post').get(function(req,res,next){
+	res.render('post',{title:'POST'});
+}).post(function(req,res,next){
+	var path=req.body.path;
+	var param=JSON.parse(req.body.param);
+	var oauth_token=req.body.token;
+	var oauth_token_secret=req.body.secret;
+	var options={
+	  url:'http://api.fanfou.com/'+path,
+	  oauth_token:oauth_token,
+	  oauth_token_secret:oauth_token_secret,
+	  parameters:param
+	};
+	oa.post(options,function(error,data){
+	    if(error)
+	    {
+	      res.json({
+	        'status':0,
+	        'reason':error
+	      }); 
+	    }
+	    else{
+	      res.json({
+	      	'status':1,
+	        "result":data
+	      }); 
+	    }
+	}); 
+});
+router.route('/get').get(function(req,res,next){
+	res.render('get',{title:'get'});
+}).post(function(req,res,next){
+	var path=req.body.path;
+	var param=JSON.parse(req.body.param);
+	var oauth_token=req.body.token;
+	var oauth_token_secret=req.body.secret;
+	var options={
+	  url:'http://api.fanfou.com/'+path,
+	  oauth_token:oauth_token,
+	  oauth_token_secret:oauth_token_secret,
+	  parameters:param
+	};
+	oa.get(options,function(error,data){
+	    if(error)
+	    {
+	      res.json({
+	        'status':0,
+	        'reason':error
+	      }); 
+	    }
+	    else{
+	      res.json({
+	      	'status':1,
+	      	"result":data
+	      }); 
+	    }
+	}); 
+});
+module.exports = router;
